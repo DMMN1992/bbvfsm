e2e27e8afb40f49c5148316d1e630f6e5b57d56f
weibel.lukas@gmail.com <weibel.lukas@gmail.com@70b33aff-6d51-0ea5-a8f5-491d994888df>
2011-10-03 10:59:33 +0000

    Fixed  test. - Attention: Tests does not compile!

--- branches/2.0.0-SNAPSHOT/fsm/fsm-impl/src/main/java/ch/bbv/fsm/impl/SimpleStateMachine.java
+++ branches/2.0.0-SNAPSHOT/fsm/fsm-impl/src/main/java/ch/bbv/fsm/impl/SimpleStateMachine.java
@@ -2,6 +2,14 @@ package ch.bbv.fsm.impl;
 
 import ch.bbv.fsm.StateMachine;
 
+/**
+ * Simple state machine (used together with {@link SimpleStateMachineDefinition}.
+ * 
+ * @param <TState>
+ *            the type of the states
+ * @param <TEvent>
+ *            the type of the events
+ */
 public class SimpleStateMachine<TState extends Enum<?>, TEvent extends Enum<?>> extends
 		AbstractStateMachine<SimpleStateMachine<TState, TEvent>, TState, TEvent> {
 
